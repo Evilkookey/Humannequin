e0e33663e39024f6357a735d6c1da7461aee0419
maxmills372 <topmanmax@gmail.com>
2017-11-17 15:15:59 +0000

    retried line puzzle

--- Humannequin_Project/Assets/Scripts/Max/Line_renderer_puzzle.cs
+++ Humannequin_Project/Assets/Scripts/Max/Line_renderer_puzzle.cs
@@ -29,7 +29,7 @@ public class Line_renderer_puzzle : MonoBehaviour {
 
 //
 	//LineRenderer line_renderer2;
-	bool already_using = false;
+	public bool already_using = false;
 
 
 	// Use this for initialization
@@ -69,25 +69,24 @@ public class Line_renderer_puzzle : MonoBehaviour {
 						already_using = true;
 						current_line = red_Line;
 
-						//lines [line_index].line_renderer.SetPosition (lines [line_index].line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
 						red_Line.line_renderer.SetPosition (red_Line.line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
 
-						//line_renderer.SetPosition (counter+1, box3.transform.position);
 						hit.collider.GetComponent<Puzzle_cube> ().SetHit (true);
 
 					} 
-					else if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.blue && !blue_Line.line_complete) 
+					else if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.blue && !blue_Line.line_complete && !already_using) 
 					{ 
 						Debug.Log ("BLUEEE!");
 						line_index = 1;
+
 						already_using = true;
+
 						current_line = blue_Line;
 
-						//lines [line_index].line_renderer.SetPosition (lines [line_index].line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
+
 						blue_Line.line_renderer.SetPosition (blue_Line.line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
 
 
-						//line_renderer.SetPosition (counter+1, box3.transform.position);
 						hit.collider.GetComponent<Puzzle_cube> ().SetHit (true);
 					}
 
@@ -98,125 +97,124 @@ public class Line_renderer_puzzle : MonoBehaviour {
 			{
 				if (hit.collider.gameObject.tag == "empty")
 				{
-					if (hit.collider.GetComponent<Puzzle_cube> ().hit == false) {
-	
-						//lines[line_index].line_renderer.positionCount++;
+					if (hit.collider.GetComponent<Puzzle_cube> ().hit == false && !current_line.line_complete) 
+					{
 						current_line.line_renderer.positionCount++;
-
-						//lines [line_index].line_renderer.SetPosition (lines [line_index].line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
+					
 						current_line.line_renderer.SetPosition (current_line.line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
 						
 						hit.collider.GetComponent<Puzzle_cube> ().SetHit (true);
 
 					} 
 				}
-				if (hit.collider.gameObject.tag == "start")
+				if (hit.collider.gameObject.tag == "start" && already_using)
 				{
-					if (hit.collider.GetComponent<Puzzle_cube> ().hit == false) 
+					/*if (hit.collider.GetComponent<Puzzle_cube> ().hit == false ) 
 					{
-						if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.red) 
+						if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.red && !red_Line.line_complete && red_Line.line_renderer.positionCount > 1) 
 						{
 							already_using = true;
-							//lines[line_index].line_renderer.positionCount++;
-							//lines[line_index].line_renderer.SetPosition (lines[line_index].line_renderer.positionCount -1, hit.collider.gameObject.transform.position);
 
 							red_Line.line_renderer.positionCount++;
 							red_Line.line_renderer.SetPosition (red_Line.line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
 
 							hit.collider.GetComponent<Puzzle_cube> ().SetHit (true);
 
-	//						if (lines[line_index].line_complete == false) 
-	//						{
-	//							lines[line_index].line_complete = true;
-	//						}
 							if (red_Line.line_complete == false) {
 								red_Line.line_complete = true;
 							}
 						}
-						else if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.blue) 
+						else if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.blue && !blue_Line.line_complete && blue_Line.line_renderer.positionCount > 1) 
 						{
 							already_using = true;
-							//lines[line_index].line_renderer.positionCount++;
-							//lines[line_index].line_renderer.SetPosition (lines[line_index].line_renderer.positionCount -1, hit.collider.gameObject.transform.position);
 
 							blue_Line.line_renderer.positionCount++;
 							blue_Line.line_renderer.SetPosition (blue_Line.line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
 
 							hit.collider.GetComponent<Puzzle_cube> ().SetHit (true);
 
-	//						if (lines[line_index].line_complete == false) 
-	//						{ 
-	//							lines[line_index].line_complete = true;
-	//						}
 							if (blue_Line.line_complete == false) {
 								blue_Line.line_complete = true;
 							}
 						}
-					}
+					}*/
 				}
 			}
 			if (Input.GetMouseButtonUp (0))
 			{
-				if (red_Line.line_complete) 
+				if (hit.collider.gameObject.tag == "Finish" ) 
 				{
+					if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.red && !red_Line.line_complete && already_using) 
+					{
+						
+						Debug.Log ("DONE REd");
+						already_using = false;
 
-					Debug.Log ("DONE");
 
-//
-//					line_index++;
-//					//line_renderer[line_index] = Instantiate (line_renderer,line_renderer[0].transform) as LineRenderer;
-//					red_Line.line_renderer.positionCount = 1;
-//					red_Line.line_renderer.SetPosition (0, new Vector3 (0, 0, 0));
-//					red_Line.line_complete = false;
+
+						red_Line.line_renderer.positionCount++;
+						red_Line.line_renderer.SetPosition (red_Line.line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
+
+						hit.collider.GetComponent<Puzzle_cube> ().SetHit (true);
+
+						if (red_Line.line_complete == false) {
+							red_Line.line_complete = true;
+						}
+
+					} 
+					if (hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.blue && !blue_Line.line_complete && already_using) 
+					{ 
+						Debug.Log ("Done BLUEEE!");
+						already_using = false;
+
+						blue_Line.line_renderer.positionCount++;
+						blue_Line.line_renderer.SetPosition (blue_Line.line_renderer.positionCount - 1, hit.collider.gameObject.transform.position);
+
+						hit.collider.GetComponent<Puzzle_cube> ().SetHit (true);
+
+						if (blue_Line.line_complete == false) {
+							blue_Line.line_complete = true;
+						}
+					}
 
 				}
-				else
+
+
+				if (red_Line.line_complete) 
+				{
+					Debug.Log ("DONE");
+
+				}
+				else 
 				{
 					ResetCubes ();
 					red_Line.line_renderer.positionCount = 1;
+					already_using = false;
+
 					counter = 0;
 				
 				}
-				if (blue_Line.line_complete) 
-				{
-
-					Debug.Log ("DONE");
-
-
-//					line_index++;
-//					//line_renderer[line_index] = Instantiate (line_renderer,line_renderer[0].transform) as LineRenderer;
-//					blue_Line.line_renderer.positionCount = 1;
-//					blue_Line.line_renderer.SetPosition (0, new Vector3 (0, 0, 0));
-//					blue_Line.line_complete = false;
-
-				}
-				else
-				{
-					ResetCubes ();
-					blue_Line.line_renderer.positionCount = 1;
-					counter = 0;
-
-				}
-
-//				if (lines[line_index].line_complete) 
+//				if (blue_Line.line_complete) 
 //				{
 //
 //					Debug.Log ("DONE");
 //
 //
-//					line_index++;
-//					//line_renderer[line_index] = Instantiate (line_renderer,line_renderer[0].transform) as LineRenderer;
-//					lines[line_index].line_renderer.positionCount = 1;
-//					lines[line_index].line_renderer.SetPosition (0, new Vector3 (0, 0, 0));
-//					lines[line_index].line_complete = false;
+////					line_index++;
+////					//line_renderer[line_index] = Instantiate (line_renderer,line_renderer[0].transform) as LineRenderer;
+////					blue_Line.line_renderer.positionCount = 1;
+////					blue_Line.line_renderer.SetPosition (0, new Vector3 (0, 0, 0));
+////					blue_Line.line_complete = false;
 //
 //				}
-//				else
+//				else if(already_using)
 //				{
 //					ResetCubes ();
-//					lines[line_index].line_renderer.positionCount = 1;
+//					blue_Line.line_renderer.positionCount = 1;
+//					already_using = false;
+//
 //					counter = 0;
-//				
+//
 //				}
 			}
 		}
---
+++ Humannequin_Project/Assets/Scripts/Max/TestScripts/LinePuzzleAgain.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinePuzzleAgain : MonoBehaviour {
+
+	float raycast_distance = 200.0f;
+
+	[System.Serializable]
+	public struct lines_struct
+	{
+		public LineRenderer line_renderer;
+		public bool line_complete;
+
+	};
+
+	//Line which gets set when you press the start cube
+	public lines_struct current_line;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Cast ray from camera 
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3((Screen.width / 2), (Screen.height / 2)));
+		Debug.DrawRay (ray.origin, ray.direction);
+
+		if (Physics.Raycast (ray.origin, ray.direction, out hit, raycast_distance)) 
+		{
+			if(Input.GetMouseButton(0))
+			{
+				//if cube is a starting cube
+				if (hit.collider.gameObject.tag == "start") 
+				{
+					//If cube is red
+					if(hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.red)
+					{
+						//Set the line renderer to the red one
+						current_line.line_renderer = GameObject.Find("LineRendererRed").GetComponent<LineRenderer>();
+
+						current_line.line_renderer.SetPosition(current_line.line_renderer.positionCount -1, hit.collider.gameObject.transform.position);
+					}
+				}
+
+				//if cube is a starting cube
+				if (hit.collider.gameObject.tag == "empty") 
+				{
+					if(current_line.line_renderer != null)
+					{
+						//if cube is not already hit
+						if(hit.collider.gameObject.GetComponent<Puzzle_cube>().hit == false)
+						{
+							hit.collider.gameObject.GetComponent<Puzzle_cube>().SetHit(true);
+
+							current_line.line_renderer.positionCount++;
+							current_line.line_renderer.SetPosition(current_line.line_renderer.positionCount -1, hit.collider.gameObject.transform.position);
+						}
+					}
+				}
+
+			}
+			if(Input.GetMouseButtonUp(0))
+			{
+				//if cube is a finish cube
+				if (hit.collider.gameObject.tag == "Finish") 
+				{
+					//If cube is red
+					if(hit.collider.gameObject.GetComponent<Renderer> ().material.color == Color.red)
+					{
+						current_line.line_renderer.positionCount++;
+						current_line.line_renderer.SetPosition(current_line.line_renderer.positionCount -1, hit.collider.gameObject.transform.position);
+					}
+
+				}
+
+				current_line.line_renderer = null;
+			}
+		}
+	}
+}
