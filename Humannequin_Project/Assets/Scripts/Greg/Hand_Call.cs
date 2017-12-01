d0fbaac6acda8574e498db3069abb38311b518a0
Evilkookey <evilkookey64@gmail.com>
2017-12-01 14:00:36 +0000

    Added hand prefabs
    Hand prefabs with colours, scripts and colliders

---
+++ Humannequin_Project/Assets/Scripts/Greg/Hand_Call.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Call the script on the controller to tell it there has been 
+
+public class Hand_Call : MonoBehaviour 
+{
+	// When something enters the trigger
+	void OnTriggerEnter(Collider other)
+	{
+		// Call the parent object
+		gameObject.SendMessageUpwards("ParentOnTriggerEnter", other);
+	}
+
+	// When something exits the trigger
+	void OnTriggerExit(Collider other)
+	{
+		// Call the parent object
+		gameObject.SendMessageUpwards("ParentOnTriggerExit", other);
+	}
+}
