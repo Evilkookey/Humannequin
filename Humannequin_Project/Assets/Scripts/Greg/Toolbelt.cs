7c18f96fffc8dd89c620c779e436287e37b480cd
Evilkookey <evilkookey64@gmail.com>
2018-02-16 12:55:19 +0000

    added take tool function to new toolbelt script

--- Humannequin_Project/Assets/Scripts/Greg/Toolbelt.cs
+++ Humannequin_Project/Assets/Scripts/Greg/Toolbelt.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Toolbelt : MonoBehaviour 
 {
+	struct tool
+	{
+		public GameObject tool_object;
+		public bool is_in_belt;
+		public bool is_acquired;
+	}
 
-	// Use this for initialization
+	// The tools
+	static tool wrench, torch, screwdriver, pliers;
+
+	// An array of tools
+	tool[] tools = new tool[4] {wrench, torch, screwdriver, pliers}; 
+
+	/*// Use this for initialization
 	void Start () 
 	{
 		
@@ -15,5 +27,57 @@ public class Toolbelt : MonoBehaviour
 	void Update () 
 	{
 		
+	}*/
+
+	bool Take_Tool (string tool_name)
+	{
+		int tool_pointer;
+
+		// Make tool name lower case
+		tool_name = tool_name.ToLower();
+
+		switch (tool_name)
+		{
+		case "wrench":
+			tool_pointer = 0;
+			break;
+		case "torch":
+			tool_pointer = 1;
+			break;
+		case "screwdriver":
+			tool_pointer = 2;
+			break;
+		case "pliers":
+			tool_pointer = 3;
+			break;
+		default:
+			return false;
+		}
+
+		// Check if tool is active and aquired
+		if (tools[tool_pointer].is_acquired && tools[tool_pointer].is_in_belt)
+		{
+			// Deactivate tool
+			tools[tool_pointer].tool_object.SetActive(false);
+
+			// Take tool out of belt
+			tools[tool_pointer].is_in_belt = false;
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool Return_Tool (string tool_name)
+	{
+
+	}
+
+	void New_Tool(string tool_name)
+	{
+
 	}
 }
