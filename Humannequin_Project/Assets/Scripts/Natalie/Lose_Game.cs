509a30f211ee5f609333dd6376a1e4a6ee61fc5e
Natalie Baker-Hall <nataliebhall@live.co.uk>
2018-02-23 18:08:09 +0000

    Edited the level layout to the new design. Added a collider for the win state. Started to write scripts for the win and lose conditions. Added some lights into the corrider. Moved the Main Menu to match up with the new level layout.
    TO DO:
    - Finish the Win condition script
    - Add the Lose condition to the script and add that to the player
    - Edit the scene_controller script to enable start wall once player has entered the Final_Prototype scene
    - Begin working on notes
    - Have a nap

---
+++ Humannequin_Project/Assets/Scripts/Natalie/Lose_Game.cs
@@ -0,0 +1,22 @@
+﻿// LOSE_GAME.CS
+// NATALIE BAKER-HALL
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lose_Game : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+	{
+		// If the current game state is "LOSE" then the player has died
+		if (Game_State_Controller.current_state == Game_State_Controller.Game_States.LOSE) 
+		{
+			// TODO
+			// Die
+			// Fade to black
+			// Display lose screen
+			// Reload Main_Menu
+		}
+	}
+}
