66c6d8ec5dd58c69a87037eeb060bb6e271423ae
maxmills372 <topmanmax@gmail.com>
2018-02-26 15:50:55 +0000

    not much tbh

--- Humannequin_Project/Assets/Scripts/Max/Note_Type.cs
+++ Humannequin_Project/Assets/Scripts/Max/Note_Type.cs
@@ -13,11 +13,13 @@ public class Note_Type : MonoBehaviour {
 
 	public type_ Note;
 	public bool picked_up;
+	GameObject pause_controller;
 
 	// Use this for initialization
 	void Start () 
 	{
 		picked_up = false;
+		pause_controller = GameObject.Find("pause_menu_controller");
 	}
 	
 	// Update is called once per frame
@@ -33,7 +35,10 @@ public class Note_Type : MonoBehaviour {
 			picked_up = true;
 
 			// Send message to pause menu contrller here
-
+			if(pause_controller)
+			{
+				pause_controller.SendMessage("Activate", Note);
+			}
 		}
 	}
 }
