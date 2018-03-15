a16ae6ded22efb7d5ed99d2530ca3e8c7f0927fb
maxmills372 <topmanmax@gmail.com>
2018-03-15 13:54:12 +0000

    imported new mannequins and models

---
+++ Humannequin_Project/Assets/Mannequin_Animated.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mannequin_Animated : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * 2.0f / 1000);
+	}
+}
