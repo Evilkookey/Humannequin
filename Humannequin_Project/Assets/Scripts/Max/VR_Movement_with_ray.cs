ada8a34c996f3d78882c9ca707ba4f0522b9f0c6
maxmills372 <topmanmax@gmail.com>
2018-03-19 13:19:38 +0000

    made sprinting a thing

--- Humannequin_Project/Assets/Scripts/Max/VR_Movement_with_ray.cs
+++ Humannequin_Project/Assets/Scripts/Max/VR_Movement_with_ray.cs
@@ -10,9 +10,12 @@ public class VR_Movement_with_ray : MonoBehaviour
 
 	public Transform rig;				//SteamVr CameraRig
 	public float speed;					//Speed for moving to player 
+    public float counter;
+    public int no_of_presses = 0;
+    public bool double_tap = false; 
 
-	//Button initialisation
-	private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+    //Button initialisation
+    private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
 
 	// Defining controller and tracked object
 	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
@@ -51,8 +54,24 @@ public class VR_Movement_with_ray : MonoBehaviour
 				return;
 			}
 
-			// Get input from the controller object
-			var device = SteamVR_Controller.Input((int)trackedObj.index);
+            // Get touch input from the controller
+            if (controller.GetPressDown(touchpad))
+            {
+                no_of_presses ++;
+
+               
+            }
+         
+            if(no_of_presses<0)
+            {
+                counter += Time.time;
+            }
+
+            
+           
+
+            // Get input from the controller object
+            var device = SteamVR_Controller.Input((int)trackedObj.index);
 
 			// Get touch input from the controller
 			if (controller.GetPress(touchpad))
@@ -64,12 +83,41 @@ public class VR_Movement_with_ray : MonoBehaviour
 				// If there is a rig
 				if (rig != null)
 				{
-					// Move rig in position of the axis with delta time and speed variables
-					rig.Translate(((Vector3.right * axis.x) + (Vector3.forward * axis.y)) * Time.deltaTime * speed/*, Space.Self*/);
-					rig.position = new Vector3(rig.position.x, rig.position.y, rig.position.z);
+
+                    no_of_presses++;
+                    if (no_of_presses == 2)
+                    {
+                        if (counter < 0.5f)
+                        {
+
+                            double_tap = true;
+                        }
+                        else
+                        {
+                            no_of_presses = 0;
+                            double_tap = false;
+                        }
+
+                        counter = 0;
+                    }
+
+                    if (!double_tap)
+                    {
+                        // Move rig in position of the axis with delta time and speed variables
+                        rig.Translate(((Vector3.right * axis.x) + (Vector3.forward * axis.y)) * Time.deltaTime * speed/*, Space.Self*/);
+                        rig.position = new Vector3(rig.position.x, rig.position.y, rig.position.z);
+                    }
+                    else
+                    {
+                        // Move rig in position of the axis with delta time and double speed variables - Sprint
+                        rig.Translate(((Vector3.right * axis.x) + (Vector3.forward * axis.y)) * Time.deltaTime * speed * 2.0f/*, Space.Self*/);
+                        rig.position = new Vector3(rig.position.x, rig.position.y, rig.position.z);
+
+                    }
 				}
 			}
 		}
+
 
 
 		// Fade out screen if head is placed in an object
