85569da932b5436e2da2cccbc04d8cc1f9d73834
Evilkookey <evilkookey64@gmail.com>
2018-03-23 13:23:29 +0000

    made telemann work with light, not with players vision
    The mannequin with move regardless of where the player is looking but the lights go out so the player canot see this.

--- Humannequin_Project/Assets/Scripts/Greg/Teleport_Mannequin_timer.cs
+++ Humannequin_Project/Assets/Scripts/Greg/Teleport_Mannequin_timer.cs
@@ -15,27 +15,23 @@ public class Teleport_Mannequin_timer : MonoBehaviour {
 	public GameObject CameraRigPlayer, FPSController; // Used to determine if the player is in VR or using the FP controller
 						
 	int index = 1;								// Index variable for the array, starts at 1 so it ignores the first one
-	bool enemy_moved;							// Checks if the mannequin can actually move and 
+	//bool enemy_moved;							// Checks if the mannequin can actually move and 
 	Vector3 target_postition;					// Players position
 
 	float timer;								// A timer for the mannequin's movement
 	public float move_time = 10.0f;				// The amount of time before the mannequin moves
-	Vector3 current_position;					// The position the mannequin should be in 
+	//Vector3 current_position;					// The position the mannequin should be in 
 
-	public bool is_enabled;							// The mannequin is enabled when it can start moving towards the player
-	public Light flickering_light;				// Enemy light that will affect the mannequins position
-	public GameObject line_puzzle; 				// Used to check if the player has completed the puzzle
+	public bool is_enabled;						// The mannequin is enabled when it can start moving towards the player
+	public Light flickering_light;              // Enemy light that will affect the mannequins position
+    public float light_off_time;                // The length of time the light will be off
+    public GameObject line_puzzle; 				// Used to check if the player has completed the puzzle
 
 	// Use this for initialization
 	void Start () 
 	{
-		// Initialise variables
-		enemy_moved = false;
-
 		// Looks through all the positions in the enemy positions object and adds it to the array
 		enemy_positions = position_parent.GetComponentsInChildren<Transform> ();
-
-		current_position = this.transform.position;
 
 		// Mannequin is disabled to start
 		is_enabled = false;
@@ -45,27 +41,17 @@ public class Teleport_Mannequin_timer : MonoBehaviour {
 		FPSController = GameObject.Find("FPSController");
 
 		player = CameraRigPlayer.transform;
-
-		/*//Switch who the player is for debugging
-		if (FPSController.activeInHierarchy == true || CameraRigPlayer == null) 
-		{
-			print ("yes");
-			player = FPSController.transform;
-
-		} else if (CameraRigPlayer.activeInHierarchy == true || FPSController == null ) 
-		{
-			print ("yes");
-			player = CameraRigPlayer.transform;
-		}*/
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
+        // If game is in play state
 		if (Game_State_Controller.current_state == Game_State_Controller.Game_States.PLAYING) 
 		{		
-			// Check if enabled
-			if (is_enabled) {
+			// Check if mannequin is enabled and line puzzle is incomplete
+			if (is_enabled && !line_puzzle.GetComponent<Wire_Puzzle_VR>().finished)
+            {
 				// Update mannequin
 				Mannequin_Update ();
 			}
@@ -75,64 +61,38 @@ public class Teleport_Mannequin_timer : MonoBehaviour {
 	// Mannequin update is called once per frame when mannequin can move
 	void Mannequin_Update () 
 	{
-
 		// Run the timer
 		timer += Time.deltaTime;
-
-		//print(timer);
 
 		// Check if the timer has exceeded move time
 		if (timer >= move_time)
 		{
-			// If the enemy has not moved already
-			if (!enemy_moved) 
+			// If the array is still full
+			if(index < enemy_positions.Length) 
 			{
-				// If the array is still full
-				if(index < enemy_positions.Length) 
-				{
-					// Move the enemy
-					Move_enemy ();
-				}
-				else
-				{
-					if(!line_puzzle.GetComponent<Wire_Puzzle_VR>().finished)
-					{
-						// Enemy is at last position, so it should kill you here 
-						Game_State_Controller.Lose_Game();
+				// Move the enemy
+				Move_enemy ();
+			}
+			else
+			{
+				// Enemy is at last position, so it should kill you here 
+				Game_State_Controller.Lose_Game();
 
-                        print("MannequinKills");
-					}
-
-
-				}
+                print("MannequinKills");
 			}
 			// Reset timer
 			timer = 0.0f;
-		}
-		else
-		{
-			// Reset bools if light comes back on
-			enemy_moved = false;
-		}
-
-		// If mannequin is unseen
-		if (!this.GetComponent<Renderer> ().isVisible)
-		{
-
-			// Set mannequin position
-			this.transform.position = current_position;
-
 		}
 	}
 
 	//Used to move the enemy
 	void Move_enemy()
 	{
-		// Enemy has now been moved (or is about to be)
-		enemy_moved = true;
+        // Turn out the lights
+        StartCoroutine("Light_Off", light_off_time);
 
-		// Move the enemy to the next set location
-		current_position = enemy_positions[index].position;
+        // Move the enemy to the next set location
+        this.transform.position = enemy_positions[index].position;
 
 		//Get the look position of the player
 		target_postition = new Vector3 (player.position.x, 
@@ -156,4 +116,19 @@ public class Teleport_Mannequin_timer : MonoBehaviour {
 		print("Get enabled");
 		is_enabled = true;
 	}
+
+    // Light off timer
+    IEnumerator Light_Off (float t)
+    {
+        // Turn light off
+        flickering_light.enabled = false;
+
+        // Wait
+        yield return new WaitForSeconds(t);
+
+        //Turn light back on
+        flickering_light.enabled = true;
+
+        yield return null;
+    }
 }
